f7d52ffeddc106f49b319691bde4005599c57055
Andreas Fuchs <asf@boinkor.net>
2009-03-31 17:40:13 +0200

    Add a hexagram(?) to the pentagram example.

--- examples/pentagram.clj
+++ examples/pentagram.clj
@@ -9,4 +9,14 @@
          ;; golden ratio, apparently.
          (* inner-radius (+ 1 (/ (+ 1 (Math/sqrt 5)) 2))))))
 
+(defn draw-hexagram [inner-radius]
+  (def-tree [(window) "6 points! Whee!"]
+   (star 6 inner-radius
+         ;; ratio between the inner and outer radius is 1 +
+         ;; golden ratio, apparently.
+         (* inner-radius (Math/sqrt 3)))))
+
 (draw-pentagram 15)
+
+(originate [(* 15 (+ 1 (/ (+ 1 (Math/sqrt 5)) 2))) 0 0]
+ (draw-hexagram 10))
